887cc3dd386617be6ffc018d4ac062b19c30ba2f
husniramdani <husniramdani18@gmail.com>
2024-01-15 13:31:41 +0700

    update setup

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,41 @@
+return {
+  { "craftzdog/solarized-osaka.nvim" },
+  { "lmintmate/blue-mood-vim" },
+  { "vim-scripts/blue.vim" },
+  { "luisiacc/gruvbox-baby" },
+  { "morhetz/gruvbox" },
+  { "rebelot/kanagawa.nvim" },
+  { "rose-pine/neovim" },
+  {
+    "tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
+
+-- blue-mood-vim
+-- return {
+--   { "lmintmate/blue-mood-vim" },
+-- {
+--   "LazyVim/LazyVim",
+--   opts = {
+--     colorscheme = "blue-mood",
+--   },
+-- },
+-- }
+
+-- blue vim
+-- return {
+--   { "vim-scripts/blue.vim" },
+-- {
+--   "LazyVim/LazyVim",
+--   opts = {
+--     colorscheme = "blue",
+--   },
+-- },
+-- }
